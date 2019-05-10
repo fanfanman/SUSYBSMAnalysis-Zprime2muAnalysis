68770d5cdf1a739f54de64373957fd022150c58c
Fan <aprilzhang1994@gmail.com>
2019-05-10 13:21:37 -0500

    ebe

--- test/runAnalysis/crabConfig.py
+++ test/runAnalysis/crabConfig.py
@@ -1,21 +1,23 @@
 
 from CRABClient.UserUtilities import config
 config = config()
-config.General.requestName = 'dileptonAna_resolution_dyPt50To150_2Jets_2017_2017_whystuck'
+config.General.requestName = 'dileptonAna_resolution_SingleMuonRun2017F-31Mar2018-v1_data2017ebe'
 config.General.workArea = 'crab2'
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'cmssw_cfg.py'   
-config.Data.inputDataset =  '/DY2JetsToLL_M-50_LHEZpT_50-150_TuneCP5_13TeV-amcnloFXFX-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+config.Data.inputDataset =  '/SingleMuon/Run2017F-31Mar2018-v1/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.publication = False
-config.Data.outputDatasetTag = 'dileptonAna_resolution_dyPt50To150_2Jets_2017_whystuck'
-config.Data.outLFNDirBase = '/store/user/zhangfa/DYMC2017resNOMUOrerun'
-#config.Data.ignoreLocality = True
+config.Data.outputDatasetTag = 'dileptonAna_resolution_SingleMuonRun2017F-31Mar2018-v1_data2017ebe'
+config.Data.outLFNDirBase = '/store/user/zhangfa/Data2017eventbyevent'
+config.Data.ignoreLocality = True
 #config.General.instance = 'preprod' 
+config.Site.whitelist = ["T2_US_*"]
 config.Site.storageSite = 'T3_US_FNALLPC'
 config.JobType.maxMemoryMB  = 8000
 
-config.Data.splitting = 'EventAwareLumiBased'
+config.Data.splitting = 'LumiBased'
 config.Data.totalUnits = -1
-config.Data.unitsPerJob  = 500000
+config.Data.unitsPerJob = 400
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_MuonPhys.txt'
 
